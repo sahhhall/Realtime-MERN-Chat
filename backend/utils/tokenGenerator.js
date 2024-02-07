fb0436df3de6bf00d3d9f8f8fc8f8af1e6eec39b
sahhhall <sahalvavoor313@gmail.com>
2024-02-07 08:55:09 +0530

    token bug

--- backend/utils/tokenGenerator.js
+++ backend/utils/tokenGenerator.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 
 const generateToken = (userId) => {
     return jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn: 86400
+        expiresIn: 326400
     })
 }
 
